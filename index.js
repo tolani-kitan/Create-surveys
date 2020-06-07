4f1e9533a7d6bcd1a797abce552e328be0732c41
Tolani-kitan <omotolanimodupe@gmail.com>
2020-06-07 02:15:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(4000);
